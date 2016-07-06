31c102c47c47a5228ac601385acaa44c4e908f2b
bingbo <ibingbo.zh@gmail.com>
2016-07-06 18:59:05 +0800

    up

--- runtime/Smarty/compile/453c149bbab5946ba1e5b66b1f1380a6175baf0f_0.file.index.tpl.php
+++ runtime/Smarty/compile/453c149bbab5946ba1e5b66b1f1380a6175baf0f_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30-dev/72, created on 2016-07-06 10:34:11
+/* Smarty version 3.1.30-dev/72, created on 2016-07-06 10:55:31
   from "/Users/zhangbingbing/Work/nginx/html/yiidemo/views/index/index.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30-dev/72',
-  'unifunc' => 'content_577cdea319c4d7_92863909',
+  'unifunc' => 'content_577ce3a3e11064_45176315',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '453c149bbab5946ba1e5b66b1f1380a6175baf0f' => 
     array (
       0 => '/Users/zhangbingbing/Work/nginx/html/yiidemo/views/index/index.tpl',
-      1 => 1467463491,
+      1 => 1467802372,
       2 => 'file',
     ),
   ),
@@ -20,27 +20,41 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_577cdea319c4d7_92863909 (Smarty_Internal_Template $_smarty_tpl) {
+function content_577ce3a3e11064_45176315 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
-<div>
-<table>
-<tr><th>id</th><th>name</th><th>password</th></tr>
-<?php
+<html>
+    <head>
+        <link href="/static/css/bootstrap.css" rel="stylesheet"/>
+        <link href="/static/css/site.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div>
+            <table>
+                <tr><th>id</th><th>name</th><th>password</th></tr>
+                <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['users']->value, 'user', false, 'key');
 if ($_from !== null) {
 foreach ($_from as $_smarty_tpl->tpl_vars['key']->value => $_smarty_tpl->tpl_vars['user']->value) {
 ?>
-<tr><td><?php echo $_smarty_tpl->tpl_vars['user']->value['id'];?>
+                <tr><td><?php echo $_smarty_tpl->tpl_vars['user']->value['id'];?>
 </td><td><?php echo $_smarty_tpl->tpl_vars['user']->value['name'];?>
 </td><td><?php echo $_smarty_tpl->tpl_vars['user']->value['password'];?>
 </td></tr>
-<?php
+                <?php
 }
 }
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
 ?>
 
-</table>
-</div>
+            </table>
+        </div>
+    </body>
+    <?php echo '<script'; ?>
+ src="/static/js/jquery.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="/static/js/bootstrap.js"><?php echo '</script'; ?>
+>
+</html>
 <?php }
 }
--- views/index/index.tpl
+++ views/index/index.tpl
@@ -1,8 +1,18 @@
-<div>
-<table>
-<tr><th>id</th><th>name</th><th>password</th></tr>
-{foreach $users as $key => $user}
-<tr><td>{$user.id}</td><td>{$user.name}</td><td>{$user.password}</td></tr>
-{/foreach}
-</table>
-</div>
+<html>
+    <head>
+        <link href="/static/css/bootstrap.css" rel="stylesheet"/>
+        <link href="/static/css/site.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div>
+            <table>
+                <tr><th>id</th><th>name</th><th>password</th></tr>
+                {foreach $users as $key => $user}
+                <tr><td>{$user.id}</td><td>{$user.name}</td><td>{$user.password}</td></tr>
+                {/foreach}
+            </table>
+        </div>
+    </body>
+    <script src="/static/js/jquery.js"></script>
+    <script src="/static/js/bootstrap.js"></script>
+</html>
